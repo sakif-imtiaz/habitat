b9d25511ad51f32c055d0090cf368b62dd74b85b
Sakif Imtiaz <limsoup@gmail.com>
2016-05-02 16:14:32 -0400

    added a cart and some ui. will do user accounts and signup next

--- volunteer.js
+++ volunteer.js
@@ -5,6 +5,17 @@ BuildList = new Mongo.Collection('builds');
 
 if(Meteor.isClient){
   //BUILDS
+
+  Router.route('/', function(){
+    this.render('home');
+  },  {name:'home'});
+  Router.route('/manage',{name:'manage'});
+  Router.route('/volunteer',{name:'volunteer'});
+  Router.configure({
+    layoutTemplate: 'ApplicationLayout'
+  });
+
+  //builds
   if(true){
     Template.builds.helpers({
       'builds': function(){
@@ -24,7 +35,6 @@ if(Meteor.isClient){
 
     Template.builds.events({
       'click .build': function(){
-        console.log(this._id);
         if(this._id)
           Session.set('selectedBuild', this._id == Session.get('selectedBuild') ? null : this._id);
       },
@@ -64,6 +74,31 @@ if(Meteor.isClient){
     });
   }
 
+  if(true){
+    Template.cart.helpers({
+      'signed_up_shifts' : function() {
+        var signed_up_shifts_ids = Session.get('signed_up_shifts');
+        console.log(signed_up_shifts_ids)
+        if(signed_up_shifts_ids)
+          return ShiftList.find({_id: {$in: (signed_up_shifts_ids || []) }});
+        else
+          return [];
+      }
+    });
+    // Template.shifts.events({
+    //   'click .shift': function(){
+    //     if(this._id)
+    //       Session.set('selectedShift', this._id == Session.get('selectedShift') ? null : this._id);
+    //     else
+    //       Session.set('selectedShift', this._id);
+    //   },
+    //   'click .remove': function(){
+    //     var selectedShift = Session.get('selectedShift');
+    //     ShiftList.remove(selectedShift);
+    //   }
+    // });
+  }
+
   //SHIFTS
   if(true){
     Template.shifts.helpers({
@@ -76,20 +111,78 @@ if(Meteor.isClient){
         if(selectedShift == shiftID)
           return "selected-shift";
       },
-      'showSelectedShift' : function () {
-        var selectedPlayer = Session.get('selectedShift');
-        return ShiftList.find({}, {sort: { name: -1}});
+      'isInCart' : function(){
+        console.log(Session.get('signed_up_shifts'));
+        return _.contains(Session.get('signed_up_shifts'), this._id);
       },
+      'showSelectedShift' : function () {
+        var selectedShift = Session.get('selectedShift');
+
+        return ShiftList.findOne(selectedShift, {sort: { name: -1}});
+      },
+      'allowSignup': function () {
+        // var controller = Iron.controller();
+        return true;
+        },
       'getBuildName' : function(buildID) {
-        //console.log
+        //console.returnfa
         return BuildList.findOne(buildID).name;
+      },
+      'grouped_shifts' : function () {
+        var shifts = ShiftList.find({}, {sort: {date: 1}}).fetch();
+        var grouped_shifts = _.groupBy(shifts, function(shift){
+          return shift.build;
+        });
+        var paired = _.pairs(grouped_shifts);
+        //debugger;
+        var grouped = _.map(paired, function  (kv) {
+          console.log("***************");
+          console.log(kv);
+          console.log(BuildList.findOne(kv[0]));
+          return {
+            build : BuildList.findOne(kv[0]).name,
+            shifts : kv[1]
+          };
+        });
+        return grouped;
+      },
+      'build_name': function(shift_group){
+        return BuildList.findOne(shift_group[1][0].build).name;
+      },
+      'formatted_date': function(shift){
+        var fd = [shift.date.getMonth()+1, shift.date.getDate()+1, shift.date.getFullYear()].join('/');
+        return fd;
       }
     });
 
     Template.shifts.events({
-      'click .shift': function(){
-        if(this._id)
-          Session.set('selectedShift', this._id == Session.get('selectedShift') ? null : this._id);
+      'click .shift': function(e){
+        if(e.target.name == 'add_to_cart'){
+          if(this._id)
+            Session.set('selectedShift', this._id == Session.get('selectedShift') ? null : this._id);
+          else
+            Session.set('selectedShift', this._id);
+        }
+      },
+      'change .add_to_cart': function(e){
+       // debugger;
+        var existing_signed_up_shifts_array
+        if(!(_.isArray(Session.get('signed_up_shifts'))))
+            existing_signed_up_shifts_array =  [];
+        else
+          existing_signed_up_shifts_array = Session.get('signed_up_shifts');
+        var checked = $(e.target).is(":checked");
+        if(checked){
+          // if(Session.get('signed_up_shifts')){
+          //   Session.set(Session.get('signed_up_shifts').push(this._id));
+          // }else {
+          existing_signed_up_shifts_array.push(this._id);
+          Session.set('signed_up_shifts', existing_signed_up_shifts_array.push(this._id));
+          //}
+        }else
+          _.without(existing_signed_up_shifts_array, this._id)
+          Session.set('signed_up_shifts', existing_signed_up_shifts_array);
+        return false;
       },
       'click .remove': function(){
         var selectedShift = Session.get('selectedShift');
@@ -107,8 +200,35 @@ if(Meteor.isClient){
       'builds' : function () {
         //var selectedShift = Session.get('selectedShift');
         return BuildList.find();
+      },
+      'showSelectedShiftDate' : function () {
+        var selectedShiftID = Session.get('selectedShift');
+        if(!selectedShiftID)
+          return null;
+        var selectedShift = ShiftList.findOne(selectedShiftID);
+        var ssd = new Date(selectedShift.date);
+
+        var monthNum = ssd.getMonth()+1;
+        var monthStr = monthNum > 9 ? monthNum + '' : '0'+monthNum;
+
+        var dateNum = ssd.getDate()+1;
+        var dateStr = dateNum > 9 ? dateNum + '' : '0'+dateNum;
+        var fds =  [ssd.getFullYear(), monthStr, dateStr].join('-');
+        return fds;
+      },
+      'matchesSelectedShiftBuildId': function (val) {
+        
+        var selectedShiftID = Session.get('selectedShift');
+        if(!selectedShiftID)
+          return null;
+        var selectedShift = ShiftList.findOne(selectedShiftID);
+        return selectedShift.build == val;
       }
     });
+
+    // Template.shift_form.onRendered(function(){
+
+    // });
 
     Template.shift_form.events({
       'submit form': function (e) {
@@ -118,7 +238,7 @@ if(Meteor.isClient){
         var t = e.target;
         var formData = {
           'name': t.name.value,
-          'date': Date(t.date.value),
+          'date': new Date(t.date.value),
           'build': t.build.value,
           'time': t.time.value
         };
@@ -131,5 +251,16 @@ if(Meteor.isClient){
     });
   }
 
+  if(true){
+    Template.shift_render.helpers({
+      'formatted_date': function(){
+        var fd = [this.date.getMonth()+1, this.date.getDate()+1,this.date.getFullYear()].join('/');
+        return fd;
+      },
+      'selectedShiftClass': function () {
+        return this._id == Session.get('selectedShift') ? 'selected-shift' : null;
+      }
+    })
+  }
 
 }
